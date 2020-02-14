9122a95145312183547b84350f839b6c7b936355
theo-dupuis <theodupuis08@gmail.com>
2020-02-14 14:50:07 +0100

    Add files via upload

--- predict.py
+++ predict.py
@@ -2,49 +2,43 @@
 # coding: utf-8
 
 # ### On récupère les données
-
-# In[36]:
-
-
 import pandas as pd
 import os
 
 predict = pd.read_csv("predict.csv")
 
 
-# In[37]:
 
-
+#pretraitement
 from sklearn import preprocessing
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 
+#on encode les chaines de caracteres
 le = preprocessing.LabelEncoder()
-predict = predict.astype(str)
-predict = predict.apply(le.fit_transform)
+predict = predict.fillna(0)
+predict.Product_Info_2 = le.fit_transform(predict.Product_Info_2.astype(str))
+predict.InsuredInfo_7 = le.fit_transform(predict.InsuredInfo_7.astype(str))
+predict.InsuredInfo_8 = le.fit_transform(predict.InsuredInfo_8.astype(str))
+predict.InsuredInfo_9 = le.fit_transform(predict.InsuredInfo_9.astype(str))
 
+#on normalise les dimensions
 scaler = StandardScaler()
 X = scaler.fit_transform(predict)
 
-pca = PCA(n_components= 73)
+#on réduit le nnombre de dimensions
+pca = PCA(n_components= 8)
 X = pca.fit_transform(X)
 
 
-# In[38]:
-
-
+#on recupere le modele entraine
 from sklearn.externals import joblib
 forest = joblib.load("finalized_model.sav")
 
-
-# In[39]:
-
-
-predict['Response'] = forest.predict(X)
+#on cree le fichier CSV
+predict['Response'] = forest.predict(X).round().astype(int)
 predict.to_csv (r'result.csv', index = None, header=True)
 
-
-# In[ ]:
 
 
 
